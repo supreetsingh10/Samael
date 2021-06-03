b069ba15421d1836081457b56f71cedc9ff4c474
Supreet <singh.supreet14@gmail.com>
2021-06-03 13:10:13 +0530

    Configure dir security on your end with security.json

---
+++ secure.py
@@ -0,0 +1,27 @@
+import os 
+from typing import * 
+import json, base64
+
+class Encrypt: 
+    def checkUser(self, user: str) -> dict[str, str]: 
+        with open(r'./security/shadow.json', 'r') as f: 
+            creds = json.load(f)
+            return creds[user]
+
+    def decodePass(self, password: str) -> str: 
+        return base64.b64decode(password).decode()
+
+    def encryptUserPass(self, user: str, password: str) -> None:
+        self.username = user 
+        self.password = base64.b64encode(password.encode())
+        self.dumppass = self.password.decode('ascii')
+        with open(r'./security/shadow.json', 'w') as f: 
+            self.data = {
+                    self.username: 
+                        {
+                        'username': self.username,
+                        'password': self.dumppass
+                        }
+                    }
+            json.dump(self.data, f)
+        return None
