2bc7b635db2fb6831532484bf86a0f8b648b4e39
Supreet <singh.supreet14@gmail.com>
2021-06-06 11:37:14 +0530

    changes to secure.py

--- secure.py
+++ secure.py
@@ -11,16 +11,18 @@ class Encrypt:
     def decodePass(self, password: str) -> str: 
         return base64.b64decode(password).decode()
 
-    def encryptUserPass(self, user: str, password: str) -> None:
+    def encryptUserPass(self, user: str, password: str, host: str) -> None:
         self.username = user 
         self.password = base64.b64encode(password.encode())
         self.dumppass = self.password.decode('ascii')
+        self.host = host
         with open(r'./security/shadow.json', 'w') as f: 
             self.data = {
                     self.username: 
                         {
                         'username': self.username,
-                        'password': self.dumppass
+                        'password': self.dumppass,
+                        'host': self.host
                         }
                     }
             json.dump(self.data, f)
