6d7ef345684a3a149b9fd0ba2e7515ec0b5b10e8
Supreet <singh.supreet14@gmail.com>
2021-06-07 11:37:30 +0530

    Send mail working, need to link with intro.py

--- environ_setter.py
+++ environ_setter.py
@@ -4,8 +4,8 @@ from typing import *
 
 class Coldstart: 
     def __init__(self) -> None: 
-        os.environ['shadow'] = '~/Documents/SavedPrograms/Python/samael/samael/src/security/shadow.json'
-        os.environ['mailcache'] = '~/Documents/SavedPrograms/Python/samael/samael/src/mailcache/'
-        os.environ['drafts'] = '~/Documents/SavedPrograms/Python/samael/samael/src/drafts/'
+        os.environ['shadow'] = '/home/supreetsingh/Documents/SavedPrograms/Python/samael/samael/src/security/shadow.json'
+        os.environ['mailcache'] = '/home/supreetsingh/Documents/SavedPrograms/Python/samael/samael/src/mailcache/mailcache.txt'
+        os.environ['drafts'] = '/home/supreetsingh/Documents/SavedPrograms/Python/samael/samael/src/drafts/'
 
 
--- sendmail.py
+++ sendmail.py
@@ -3,24 +3,53 @@ from intro import Setup
 from subprocess import call
 from environ_setter import Coldstart
 from typing import *
+from shutil import copy
 
 class SendMail(): 
     cold = Coldstart()
     message = ''
+
+    def cleanUp(self, flag) -> None: 
+        with open(os.getenv('mailcache'), 'a') as f: 
+            if flag == True: 
+                print('Enter the name of the save file')
+                self.SaveFileName = input('Name : ')
+                copy(os.getenv('mailcache'), os.getenv('drafts') + self.SaveFileName)
+
+            f.truncate(0)
+
+
+    def questionSave(self) -> bool:
+        while True: 
+            try: 
+                print('Do you want to save the file in sent messages? Enter yes or no')
+                self.ans = input('Answer: ').lower().strip()
+                if self.ans[0] not in ['y', 'n']: 
+                    raise ValueError
+                else: 
+                    self.ans = self.ans[0]
+                    break
+            except ValueError: 
+                print('Enter a valid answer')
+
+        return True if self.ans == 'y' else False
+
+
     def __init__(self) -> None: 
         self.editor = os.getenv('EDITOR')
-        initial_message = ""
-        print(os.getenv('mailcache'))
+        initial_message = ''
 
-        with open('./mailcache/mailcache.txt', 'w') as f: 
+        with open(os.getenv('mailcache') , 'w+') as f: 
             f.write(initial_message)
             f.flush()
             call([self.editor, f.name])
 
-        with open('./mailcache/mailcache.txt', 'r') as f: 
+
+        with open(os.getenv('mailcache'), 'r') as f: 
             message = f.read()
 
         print(message)
+        self.cleanUp(self.questionSave())
 
 # Look at inheritance thing in Python
 sms = SendMail()
